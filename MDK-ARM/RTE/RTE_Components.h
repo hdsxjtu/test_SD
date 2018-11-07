0be35bed991fe4985045a4a1e10fabdb5d31ddb5
hdsxjtu <hdsxjtu@gmail.com>
2018-11-07 10:33:10 +0800

    nothing
    nothing

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SD_card' 
+ * Target:  'SD_card' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
